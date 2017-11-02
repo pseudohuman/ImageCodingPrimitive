bb5bcfc986face7b994428fef2deea2feb014478
Anatoly Kostiuk <dreamwatchersking@gmail.com>
2017-11-02 18:46:47 +0200

    Hello world

--- ImageCodingDemo/Program.cs
+++ ImageCodingDemo/Program.cs
@@ -13,13 +13,13 @@ namespace ImageCodingDemo
         {
             using (Bitmap bitmap = new Bitmap(200, 200))
             {
-                //bitmap.Drawline(0, 0, 199, 0, Color.Blue);
-                //bitmap.Drawline(199, 0, 199, 199, Color.Cyan);
-                //bitmap.Drawline(199, 199, 0, 199, Color.Red);
-                //bitmap.Drawline(0, 199, 0, 0, Color.Green);
-                //bitmap.Drawline(0, 0, 199, 199, Color.Green);
-                //bitmap.Drawline(0,199, 0, 199, Color.Green);
-                bitmap.DrawCircle(100, 100, 10, Color.Red);
+                bitmap.Drawline2(0, 0, 199, 0, Color.Blue);
+                bitmap.Drawline2(199, 0, 199, 199, Color.Cyan);
+                bitmap.Drawline2(199, 199, 0, 199, Color.Red);
+                bitmap.Drawline2(0, 199, 0, 0, Color.Green);
+                bitmap.Drawline2(0, 0, 199, 199, Color.Green);
+                bitmap.Drawline2(0,199, 0, 199, Color.Green);
+                bitmap.DrawCircle2(100, 100, 10, Color.Red);
                 bitmap.DrawEllipse(50,50,25,10,Color.Aqua);
                 bitmap.Polynome(10, 10, Color.Red);
                 bitmap.Save(filename, ImageFormat.Png);
@@ -44,20 +44,54 @@ namespace ImageCodingDemo
             }
            
         }
-        static void DrawlineD(this Bitmap bitmap, int StartX, int StartY, int EndX, int EndY, Color color)
+        static void Drawline2(this Bitmap bitmap, int StartX, int StartY, int EndX, int EndY, Color color)
         {
-         
-            int dx = EndX - StartX;
-            int dy = EndY - StartY;
-            int len = Math.Abs(dx) + Math.Abs(dy);
-            double ndx = (double)dx / len;
-            double ndy = (double)dy / len;
-            for (int i = 0; i <= len; i++)
+            bool steep = Math.Abs(EndY - StartY) > Math.Abs(EndX - StartX);
+            if(steep)
             {
-                int x = StartX + (int)Math.Round(i * ndx);
-                int y = StartY + (int)Math.Round(i * ndy);
-                bitmap.SetPixel(x, y, color);
+                int temp;
+                temp = StartY;
+                StartY = StartX;
+                StartX = temp;
+
+                temp = EndY;
+                EndY = EndX;
+                EndX = temp;
             }
+            if (StartX > EndX)
+            {
+                int temp;
+                temp = StartX;
+                StartX = EndX;
+                EndX = temp;
+
+                temp = StartY;
+                StartY = EndY;
+                EndX = temp;
+
+
+            }
+            int dx = EndX - StartX;
+            int dy = Math.Abs(EndY - StartY);
+          
+
+            int err = dx / 2;
+   
+            int ystep=(StartY<EndY)?1:-1;
+            int y = StartY;
+
+            for(int x = StartX;x<=EndX;x++)
+            {
+                bitmap.SetPixel(steep ? y : x, steep ? x : y,color);
+                err-= dy;
+                if (err < 0)
+                {
+                    y += ystep;
+                    err += dx;
+                }
+            }
+
+
 
         }
         static void DrawCircle(this Bitmap bitmap, int centerX, int centerY, int radius, Color color)
@@ -71,6 +105,36 @@ namespace ImageCodingDemo
                 {
                     bitmap.SetPixel(x, y, color);
                     Console.Write($" {i}");
+                }
+            }
+
+        }
+       
+        static void DrawCircle2(this Bitmap bitmap, int centerX, int centerY, int radius, Color color){
+            int x = radius;
+            int y = 0;
+            int err = 1 - x;
+            while (x >= y)
+            {
+                bitmap.SetPixel(x + centerX, y + centerY,color);
+                bitmap.SetPixel(y + centerX, x + centerY, color);
+                bitmap.SetPixel(-x + centerX, y + centerY, color);
+                bitmap.SetPixel(-y + centerX, x + centerY, color);
+                bitmap.SetPixel(x + centerX, -y + centerY, color);
+                bitmap.SetPixel(y + centerX, -x + centerY, color);
+                bitmap.SetPixel(-x + centerX, y + centerY, color);
+                bitmap.SetPixel(-y + centerX, x + centerY, color);
+                bitmap.SetPixel(-x + centerX, -y + centerY, color);
+                bitmap.SetPixel(-y + centerX, -x + centerY, color);
+                y++;
+                if (err < 0)
+                {
+                    err += 2 * y + 1;
+                }
+                else
+                {
+                    x--;
+                    err += 2 * (y - x + 1);
                 }
             }
 
